abb4c9418271223d83a8c81944817f59b324d5df
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2025-04-30 12:05:32 +0800

    changed to pnpm

---
+++ src/assets/js/variables.js
@@ -0,0 +1,13 @@
+import varss from '/src/assets/css/variables.sass';
+
+const varsjs = {
+  fontFamily: varss.$fontFamily,
+  textColor: varss.$textColor,
+  backgroundColor: varss.$backgroundColor,
+  modeProgrammerTextColor: varss.$modeProgrammerTextColor,
+  modeDesinerTextColor: varss.$modeDesinerTextColor,
+  modeWriterTextColor: varss.$modeWriterTextColor,
+
+};
+
+export default varsjs;
