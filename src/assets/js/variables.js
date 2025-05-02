b4b9fd4de3578953de9a5f2520ae6ef2bfabda0c
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2025-05-02 12:37:31 +0800

    update

--- src/assets/js/variables.js
+++ src/assets/js/variables.js
@@ -1,13 +1,51 @@
-import varss from '/src/assets/css/variables.sass';
+const backgroundColor = 'rgba(10, 23, 27, 1)';
+const textColor = 'rgb(244, 244, 244)';
+const borderColor = 'none';
 
-const varsjs = {
-  fontFamily: varss.$fontFamily,
-  textColor: varss.$textColor,
-  backgroundColor: varss.$backgroundColor,
-  modeProgrammerTextColor: varss.$modeProgrammerTextColor,
-  modeDesinerTextColor: varss.$modeDesinerTextColor,
-  modeWriterTextColor: varss.$modeWriterTextColor,
+const modeProgrammerFontFamily = 'Fira Code Variable';
+const modeDesignerFontFamily = 'Montserrat Variable';
+const modeWriterFontFamily = 'Playfair Display Variable';
 
+const modeProgrammerTextColor = 'rgba(110, 141, 159, 1)';
+const modeProgrammerBoxBorder = '5px solid rgba(32, 42, 50, 0.8)';
+const modeDesinerTextColor = 'rgba(220, 221, 183, 1)';
+const modeWriterTextColor = 'rgba(0, 0, 0, 1)';
+
+const modeProgrammerBackgroundColor = 'rgba(21, 29, 36, 1)';
+const modeDesignerBackgroundColor = 'rgba(19, 29, 29, 1)';
+const modeWriterBackgroundColor = 'rgba(208, 203, 126, 1)';
+
+const contactBoxBackgroundColor = 'rgba(10, 23, 27, 1)';
+const contactBoxTextColor = 'rgb(244, 244, 244)';
+
+const duration = 0.4
+const backgroundSize = 'cover'
+const ease = 'power2.out'
+
+const vars = {
+  backroundColor: backgroundColor,
+  textColor: textColor,
+  borderColor: borderColor,
+
+  modeProgrammerFontFamily: modeProgrammerFontFamily,
+  modeDesignerFontFamily: modeDesignerFontFamily,
+  modeWriterFontFamily: modeWriterFontFamily,
+
+  modeProgrammerTextColor: modeProgrammerTextColor,
+  modeProgrammerBoxBorder: modeProgrammerBoxBorder,
+  modeDesinerTextColor: modeDesinerTextColor,
+  modeWriterTextColor: modeWriterTextColor,
+
+  modeProgrammerBackgroundColor: modeProgrammerBackgroundColor,
+  modeDesignerBackgroundColor: modeDesignerBackgroundColor,
+  modeWriterBackgroundColor: modeWriterBackgroundColor,
+
+  contactBoxBackgroundColor: contactBoxBackgroundColor,
+  contactBoxTextColor: contactBoxTextColor,
+
+  duration: duration,
+  backgroundSize: backgroundSize,
+  ease: ease,
 };
 
-export default varsjs;
+export default vars;
