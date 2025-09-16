dd0466f8eab3deda9192d947cae5fd8fc03b5c6c
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2025-09-16 18:20:58 +0800

    Updated variables

--- src/assets/js/variables.js
+++ src/assets/js/variables.js
@@ -1,23 +1,25 @@
-const backgroundColor = 'rgba(10, 23, 27, 1)';
-const textColor = 'rgb(244, 244, 244)';
-const borderColor = 'none';
+const backgroundColor = 'rgba(10, 23, 27, 1)'
+const textColor = 'rgb(244, 244, 244)'
+const borderColor = 'none'
 
-const modeProgrammerFontFamily = 'Fira Code Variable';
-const modeDesignerFontFamily = 'Montserrat Variable';
-const modeWriterFontFamily = 'Playfair Display Variable';
+const modeProgrammerFontFamily = 'Fira Code Variable'
+const modeDesignerFontFamily = 'Montserrat Variable'
+const modeWriterFontFamily = 'Playfair Display Variable'
 
-const modeProgrammerTextColor = 'rgba(110, 141, 159, 1)';
-const modeProgrammerBoxBorder = '5px solid rgba(32, 42, 50, 0.8)';
-const modeProgrammerBoxBorderGlow = '5px solid rgba(133, 175, 223, 0.8)';
-const modeDesignerTextColor = 'rgba(220, 221, 183, 1)';
-const modeWriterTextColor = 'rgba(0, 0, 0, 1)';
+const modeProgrammerTextColor = 'rgba(110, 141, 159, 1)'
+const modeProgrammerBoxBorder = '3px solid rgba(32, 42, 50, 0.8)'
+const modeProgrammerBoxBorderGlow = '3px solid rgba(133, 175, 223, 0.8)'
+const modeDesignerTextColor = 'rgba(220, 221, 183, 1)'
+const modeDesignerBoxBorder = '3px solid rgba(121, 122, 61, 0.8)'
+const modeDesignerBoxBorderGlow = '3px solid rgba(218, 219, 129, 0.8)'
+const modeWriterTextColor = 'rgba(0, 0, 0, 1)'
 
-const modeProgrammerBackgroundColor = 'rgba(21, 29, 36, 1)';
-const modeDesignerBackgroundColor = 'rgba(19, 29, 29, 1)';
-const modeWriterBackgroundColor = 'rgba(208, 203, 126, 1)';
+const modeProgrammerBackgroundColor = 'rgba(21, 29, 36, 1)'
+const modeDesignerBackgroundColor = 'rgba(19, 29, 29, 1)'
+const modeWriterBackgroundColor = 'rgba(208, 203, 126, 1)'
 
-const contactBoxBackgroundColor = 'rgba(10, 23, 27, 1)';
-const contactBoxTextColor = 'rgb(244, 244, 244)';
+const contactBoxBackgroundColor = 'rgba(10, 23, 27, 1)'
+const contactBoxTextColor = 'rgb(244, 244, 244)'
 
 const tabBackgroundColorProgrammer = 'hsl(208, 26.30%, 11.20%)'
 const tabSelectedBackgroundColorProgrammer = 'hsl(208, 26.30%, 50.20%)'
@@ -43,6 +45,8 @@ const vars = {
   modeProgrammerBoxBorder: modeProgrammerBoxBorder,
   modeProgrammerBoxBorderGlow: modeProgrammerBoxBorderGlow,
   modeDesignerTextColor: modeDesignerTextColor,
+  modeDesignerBoxBorder: modeDesignerBoxBorder,
+  modeDesignerBoxBorderGlow: modeDesignerBoxBorderGlow,
   modeWriterTextColor: modeWriterTextColor,
 
   modeProgrammerBackgroundColor: modeProgrammerBackgroundColor,
@@ -62,6 +66,6 @@ const vars = {
   duration: duration,
   backgroundSize: backgroundSize,
   ease: ease,
-};
+}
 
-export default vars;
+export default vars
