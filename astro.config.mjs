24ff952d6a585e83a0fca207e1d563ed077bbde0
Patrick Alvin Alcala <patrick_alv@yahoo.com>
2025-09-18 16:45:54 +0800

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import compressor from "astro-compressor";
-
-import solidJs from "@astrojs/solid-js";
+import compressor from 'astro-compressor'
+import solidJs from '@astrojs/solid-js'
+import purgecss from 'astro-purgecss'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     css: {
-        preprocessorOptions: {
-            sass: {
-                api: "modern-compiler"
-            }
-        }
-    }
-},
-  integrations: [compressor({ gzip: false, brotli: true }), solidJs()],
+      preprocessorOptions: {
+        sass: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  },
+  integrations: [
+    compressor({ gzip: false, brotli: true }),
+    solidJs(),
+    robotsTxt(),
+    purgecss({
+      fontFace: true,
+      variables: true,
+    }),
+  ],
   build: {
     assets: '_patalcala',
-  }
-});
+    inlineStylesheets: 'never',
+  },
+})
